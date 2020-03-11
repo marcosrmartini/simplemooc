fe8a717debce05525baec25a85b8e8fb9efe6db1
Martini <marcosrmartini@gmail.com>
2020-03-11 05:32:19 -0300

    Implementação dashboard

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -2,13 +2,17 @@ from django.conf.urls import include
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from simplemooc.accounts.views import register
+from simplemooc.accounts.views import register, dashboard, edit, edit_password
+
 
 app_name = 'accounts'
 
 urlpatterns = [
-    #path('entrar/', LoginView, {'template_name': 'accounts/login.html'}, name='login'),
+    path('', dashboard, name='dashboard'),
     path('entrar/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('sair/', LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
     path('cadastre-se/', register, name='register'),
+    path('editar/', edit, name='edit'),
+    path('editar-senha/', edit_password, name='edit_password'),
 ]
+
